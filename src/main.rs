521f9aa8c34867eae9c6b1672f150b5265b433fa
MiikaM <miika.mikkonen5@gmail.com>
2022-12-09 21:52:16 +0200

    Parses the xml data of the assignments to a Report struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,53 @@
-use mongodb::{options::{ClientOptions, ResolverConfig}, Client};
+use bson::Timestamp;
+use chrono::{DateTime, Utc};
+use mongodb::{
+    options::{ClientOptions, ResolverConfig},
+    Client,
+};
+use serde::Deserialize;
+use serde_xml_rs::{from_str, to_string};
 use std::env;
 use std::error::Error;
 
+#[derive(Debug, Deserialize)]
+struct Report {
+    deviceInformation: DeviceInformation,
+    capture: Capture,
+}
+
+#[derive(Debug, Deserialize)]
+struct Capture {
+    drone: Vec<Drone>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Drone {
+    serialNumber: String,
+    model: String,
+    manufacturer: String,
+    mac: String,
+    ipv4: String,
+    ipv6: String,
+    firmware: String,
+    positionY: f64,
+    positionX: f64,
+    altitude: f64,
+}
+
+#[derive(Debug, Deserialize)]
+struct DeviceInformation {
+    listenRange: f32,
+    deviceStarted: String,
+    uptimeSeconds: i64,
+    updateIntervalMs: i64,
+}
+
 #[tokio::main]
-async fn main()  {
-    connect_db().await;
+async fn main() {
+    parse_xml().await;
 }
 
 async fn connect_db() -> Result<(), Box<dyn Error>> {
-
     let connection_string = env::var("MONGODB_CONNECTION_STRING")
         .expect("$MONGODB_CONNECTION_STRING has not been set!");
     // A Client is needed to connect to MongoDB:
@@ -23,5 +62,19 @@ async fn connect_db() -> Result<(), Box<dyn Error>> {
     for name in client.list_database_names(None, None).await? {
         println!("- {}", name);
     }
+    Ok(())
+}
+
+async fn parse_xml() -> Result<(), Box<dyn Error>> {
+    let response = reqwest::get("http://assignments.reaktor.com/birdnest/drones")
+        .await?
+        .text()
+        .await?;
+    let res: Report = from_str(&response).unwrap();
+    
+    for drone in res.capture.drone {
+        println!("{drone:?}");
+    }
+
     Ok(())
 }
